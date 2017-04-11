548aaf9207c176b5c1f4d720ca6619f70d00fae3
Olivier Sohn <olivier.sohn@gmail.com>
2017-04-11 14:38:50 +0200

    templatize fft : we can use float instead of doubles, the accuracy is good enough

--- include/muParserTemplateMagic.h
+++ include/muParserTemplateMagic.h
@@ -80,26 +80,26 @@ namespace mu
   template<typename T>
   struct MathImpl
   {
-    static T Sin(T v)   { return sin(v);  }
-    static T Cos(T v)   { return cos(v);  }
-    static T Tan(T v)   { return tan(v);  }
-    static T ASin(T v)  { return asin(v); }
-    static T ACos(T v)  { return acos(v); }
-    static T ATan(T v)  { return atan(v); }
-    static T ATan2(T v1, T v2) { return atan2(v1, v2); }
-    static T Sinh(T v)  { return sinh(v); }
-    static T Cosh(T v)  { return cosh(v); }
-    static T Tanh(T v)  { return tanh(v); }
-    static T ASinh(T v) { return log(v + sqrt(v * v + 1)); }
-    static T ACosh(T v) { return log(v + sqrt(v * v - 1)); }
-    static T ATanh(T v) { return ((T)0.5 * log((1 + v) / (1 - v))); }
-    static T Log(T v)   { return log(v); } 
-    static T Log2(T v)  { return log(v)/log((T)2); } // Logarithm base 2
-    static T Log10(T v) { return log10(v); }         // Logarithm base 10
-    static T Exp(T v)   { return exp(v);   }
+      static T Sin(T v)   { return std::sin(v);  }
+    static T Cos(T v)   { return std::cos(v);  }
+    static T Tan(T v)   { return std::tan(v);  }
+    static T ASin(T v)  { return std::asin(v); }
+    static T ACos(T v)  { return std::acos(v); }
+    static T ATan(T v)  { return std::atan(v); }
+    static T ATan2(T v1, T v2) { return std::atan2(v1, v2); }
+    static T Sinh(T v)  { return std::sinh(v); }
+    static T Cosh(T v)  { return std::cosh(v); }
+    static T Tanh(T v)  { return std::tanh(v); }
+    static T ASinh(T v) { return std::log(v + sqrt(v * v + 1)); }
+    static T ACosh(T v) { return std::log(v + sqrt(v * v - 1)); }
+    static T ATanh(T v) { return ((T)0.5 * std::log((1 + v) / (1 - v))); }
+    static T Log(T v)   { return std::log(v); }
+    static T Log2(T v)  { return std::log(v)/std::log((T)2); } // Logarithm base 2
+    static T Log10(T v) { return std::log10(v); }         // Logarithm base 10
+    static T Exp(T v)   { return std::exp(v);   }
     static T Abs(T v)   { return (v>=0) ? v : -v; }
-    static T Sqrt(T v)  { return sqrt(v); }
-    static T Rint(T v)  { return floor(v + (T)0.5); }
+    static T Sqrt(T v)  { return std::sqrt(v); }
+    static T Rint(T v)  { return std::floor(v + (T)0.5); }
     static T Sign(T v)  { return (T)((v<0) ? -1 : (v>0) ? 1 : 0); }
     static T Pow(T v1, T v2) { return std::pow(v1, v2); }
   };
